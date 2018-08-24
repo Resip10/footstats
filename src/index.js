2d07e73df94034c27c18db856a2ca4ccf17ee869
igor.poplavsky <igor.poplavsky@duallab.com>
2018-08-24 16:39:19 +0300

    Update of app architecture

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/app/App';
+import registerServiceWorker from './services/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
