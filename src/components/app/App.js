d5ca2e987905a28e5929597166b8d56644b503d1
Resip10 <resip@tut.by>
2018-09-10 23:45:31 +0300

    Refactoring

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,15 +1,111 @@
 import React, { Component } from 'react';
 import Header from '../header/Header';
+import classNames from 'classnames';
+import Typography from "@material-ui/core/Typography/Typography";
+import { withStyles } from '@material-ui/core/styles';
 import './App.scss';
+import PropTypes from "prop-types";
+import Drawer from '@material-ui/core/Drawer';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const drawerWidth = 240;
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+    height: '100vh'
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing.unit * 7,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing.unit * 9,
+    },
+  },
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: '0 8px',
+    ...theme.mixins.toolbar
+  }
+});
 
 class App extends Component {
+  state = {
+    menuOpen: false
+  };
+
+  handleMenuOpen = () => {
+    this.setState({
+      menuOpen: !this.state.menuOpen
+    });
+  };
+
   render() {
+    const { classes, theme } = this.props;
+
     return (
-      <div className="footstats-app">
-        <Header />
+      <div className={classes.root}>
+        <Header handleMenuOpen={this.handleMenuOpen} menuOpen={this.state.menuOpen} />
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: classNames(classes.drawerPaper, !this.state.menuOpen && classes.drawerPaperClose),
+          }}
+          open={this.state.menuOpen}
+        >
+          <div className={classes.toolbar}>
+            <Typography variant="title">
+              Menu
+            </Typography>
+          </div>
+          <List>
+            <ListItem button>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </ListItem>
+          </List>
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          <Typography noWrap>{'Main content'}</Typography>
+        </main>
       </div>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(App);
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,16 +8,10 @@ import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 //import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import Drawer from '@material-ui/core/Drawer';
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import APL_AVATAR from '../../images/apl_avatar.png'
@@ -25,15 +19,7 @@ import APL_AVATAR from '../../images/apl_avatar.png'
 import './Header.scss';
 
 const drawerWidth = 240;
-
 const styles = theme => ({
-  root: {
-    flexGrow: 1,
-    zIndex: 1,
-    overflow: 'hidden',
-    position: 'relative',
-    display: 'flex',
-  },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(['width', 'margin'], {
@@ -55,131 +41,63 @@ const styles = theme => ({
   },
   hide: {
     display: 'none',
-  },
-  drawerPaper: {
-    position: 'relative',
-    whiteSpace: 'nowrap',
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerPaperClose: {
-    overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    width: theme.spacing.unit * 7,
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing.unit * 9,
-    },
-  },
-  toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
-  content: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: theme.spacing.unit * 3,
   }
 });
 
 class Header extends Component {
-  state = {
-    menuOpen: false
-  };
-
-  handleMenuOpen = () => {
-    this.setState({
-      menuOpen: !this.state.menuOpen
-    });
-  };
-
   render() {
-    const { classes, theme } = this.props;
+    const { classes, theme, menuOpen, handleMenuOpen } = this.props;
 
     return (
-      <div className={classes.root}>
-        <AppBar title="Footstats" position="absolute"
-                className={classNames(classes.appBar, this.state.menuOpen && classes.appBarShift)}>
-          <Toolbar className="major-toolbar">
-            <IconButton
-              color="inherit"
-              aria-label="Open menu"
-              onClick={this.handleMenuOpen}
-              className={classes.menuButton}
-            >
-              <MenuIcon />
-            </IconButton>
-            <ExpansionPanel className="header-expanded">
-              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                <Grid container
-                      direction="row"
-                      spacing={Number(16)}
-                      alignItems="center">
-                  <Grid key={"headerAvatar"} item>
-                    <Avatar
-                      alt="APL"
-                      src={APL_AVATAR}
-                      className={classNames(classes.avatar, classes.bigAvatar)}
-                    />
-                  </Grid>
-                  <Grid key={"headerTitle"} item>
-                    <Typography variant="title" color="inherit">
-                      Footstats
-                    </Typography>
-                  </Grid>
+      <AppBar title="Footstats" position="absolute"
+              className={classNames(classes.appBar, menuOpen && classes.appBarShift)}>
+        <Toolbar className="major-toolbar">
+          <IconButton
+            color="inherit"
+            aria-label="Open menu"
+            onClick={handleMenuOpen}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <ExpansionPanel className="header-expanded">
+            <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+              <Grid container
+                    direction="row"
+                    spacing={Number(16)}
+                    alignItems="center">
+                <Grid key={"headerAvatar"} item>
+                  <Avatar
+                    alt="APL"
+                    src={APL_AVATAR}
+                    className={classNames(classes.avatar, classes.bigAvatar)}
+                  />
                 </Grid>
-              </ExpansionPanelSummary>
-              <ExpansionPanelDetails >
-                <div>
-                  <Typography variant="caption">
-                    Expanded
+                <Grid key={"headerTitle"} item>
+                  <Typography variant="title" color="inherit">
+                    Footstats
                   </Typography>
-                </div>
-              </ExpansionPanelDetails>
-            </ExpansionPanel>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          variant="permanent"
-          classes={{
-            paper: classNames(classes.drawerPaper, !this.state.menuOpen && classes.drawerPaperClose),
-          }}
-          open={this.state.menuOpen}
-        >
-          <div className={classes.toolbar}>
-            <Typography variant="title">
-              Menu
-            </Typography>
-          </div>
-          <List>
-            <ListItem button>
-              <ListItemIcon>
-                <InboxIcon />
-              </ListItemIcon>
-              <ListItemText primary="Inbox" />
-            </ListItem>
-          </List>
-        </Drawer>
-        <main className={classes.content}>
-          <div className={classes.toolbar} />
-          <Typography noWrap>{'Main content'}</Typography>
-        </main>
-      </div>
-    );
+                </Grid>
+              </Grid>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails >
+              <div>
+                <Typography variant="caption">
+                  Expanded
+                </Typography>
+              </div>
+            </ExpansionPanelDetails>
+          </ExpansionPanel>
+        </Toolbar>
+      </AppBar>);
   }
 }
 
 Header.propTypes = {
   classes: PropTypes.object.isRequired,
   theme: PropTypes.object.isRequired,
+  menuOpen: PropTypes.bool.isRequired,
+  handleMenuOpen: PropTypes.func.isRequired
 };
 
 export default withStyles(styles, { withTheme: true })(Header);
